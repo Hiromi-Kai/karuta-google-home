91687233b65590bc3bb2bf5025e56cc3efaef9c3
hiromi-kai <hiromikai.green@gmail.com>
2018-01-02 18:21:16 +0900

    Fix variable access

--- app/models/request_solver.rb
+++ app/models/request_solver.rb
@@ -1,10 +1,11 @@
 class RequestSolver
   attr_accessor :result
+  attr_reader :input
   #TODO 将来的にはパラメータを元に出典ゲームを区別したい
   def initialize(params)
     @input = params["result"]
     karuta = fetch_karuta(input["parameters"]["debug_karuta_id"])
-    @result = Result.new(@input, karuta)
+    @result = Result.new(input, karuta)
     @google_id = params.dig("originalRequest", "data", "user", "userId")
   end
 
