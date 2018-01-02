1ddcbb28ca4e62fe5eb5e15497937c7152484668
hiromi-kai <hiromikai.green@gmail.com>
2018-01-02 18:11:39 +0900

    Fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :google_homes do
-    post "karuta", to: "karuta#index"
-  end
+  post "karuta", to: "karuta#index"
 end
