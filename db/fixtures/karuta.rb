f348a3b4f0d6559c1a86306042b941aa67f2a0cd
hiromi-kai <hiromikai.green@gmail.com>
2018-01-02 16:50:46 +0900

    Add sample fixture

---
+++ db/fixtures/karuta.rb
@@ -0,0 +1,15 @@
+Karuta.seed do |s|
+  s.id = 1
+  s.question_text = "束ねるは星の息吹、輝ける命の奔流。受けるがいい！"
+  s.answer_text = "約束された勝利の剣"
+  s.answer_pron = "エクスカリバー"
+end
+
+Karuta.seed do |s|
+  s.id = 2
+  s.question_text = "その心臓────貰い受ける！"
+  s.question_text = "そのしんぞうもらいうける！"
+  s.answer_text = "刺し穿つ死棘の槍"
+  s.answer_pron = "ゲイ・ボルク"
+end
+
