5dce390f73a01e00819660007b64a66f891350f1
hiromi-kai <hiromikai.green@gmail.com>
2018-01-02 16:51:15 +0900

    Add karuta implements

--- app/models/karuta.rb
+++ app/models/karuta.rb
@@ -1,2 +1,17 @@
 class Karuta < ApplicationRecord
+  def question
+    question_pron || question_text
+  end
+
+  def answer
+    answer_pron || answer_text
+  end
+
+  def speech
+    "<speak>読み上げます。#{self.question}</speak>"
+  end
+  
+  def display_text
+    "読み上げます。#{self.question_text}"
+  end
 end
