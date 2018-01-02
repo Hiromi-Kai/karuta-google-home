d7d9ab85b22109ed7cc1732fc105657a014e90f2
hiromi-kai <hiromikai.green@gmail.com>
2018-01-02 17:06:02 +0900

    Add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :google_homes do
+    post "karuta", to: "karuta#index"
+  end
 end
