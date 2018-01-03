5cadabc3a365af747e67949c7f12d10a7254e63a
hiromi-kai <hiromikai.green@gmail.com>
2018-01-03 17:33:14 +0900

    Fix logic to check answer text for Japanese NP

--- app/models/karuta.rb
+++ app/models/karuta.rb
@@ -27,7 +27,8 @@ class Karuta < ApplicationRecord
   end
 
   def correct_answer?(user_answer)
-    user_answer.tr(" ", "").start_with?(answer_pron.tr("・", ""))
+    user_answer.tr(" ", "").start_with?(answer_pron.tr("・", "")) ||
+        user_answer.tr(" ", "").start_with?(answer_text.tr("・", ""))
   end
 
   def answer_judge(answer)
